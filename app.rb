c54f58e43ecd12730b4ffaed95b90f92be24c343
ice-rogue <icerogue4@gmail.comr>
2019-09-26 23:34:32 +0500

    Zzzz

--- app.rb
+++ app.rb
@@ -1,44 +1,64 @@
 require 'rubygems'
 require 'sinatra'
+require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+  @db = SQLite3::Database.new 'database.db'
+  @db.results_as_hash = true
+end
+
+before do
+  init_db
+end
 
 configure do
-  enable :sessions
+  init_db
+  @db.execute "CREATE TABLE IF NOT EXISTS 'Posts' (id INTEGER PRIMARY KEY AUTOINCREMENT, created_date DATE, content TEXT)"
+  @db.execute "CREATE TABLE IF NOT EXISTS 'Comments' (id INTEGER PRIMARY KEY AUTOINCREMENT, created_date DATE, content TEXT, post_id integer)"
 end
 
-helpers do
-  def username
-    session[:identity] ? session[:identity] : 'Hello stranger'
+get '/' do 
+  @results = @db.execute "SELECT * from Posts order by id desc"
+
+  erb :index
+end
+
+get '/new' do
+  erb :new
+end
+
+post '/new' do
+  content = params[:content]
+
+  if content.empty?
+    @error = 'Type text!'
+    return erb :new
   end
+
+  @db.execute "INSERT INTO Posts (content, created_date) values (?, datetime())", [content]
+
+  redirect to '/'
+  erb "You typed #{content}"
 end
 
-before '/secure/*' do
-  unless session[:identity]
-    session[:previous_url] = request.path
-    @error = 'Sorry, you need to be logged in to visit ' + request.path
-    halt erb(:login_form)
-  end
+get '/details/:post_id' do 
+  post_id = params[:post_id]
+
+  results = @db.execute "SELECT * from Posts where id = ?", [post_id]
+  @row = results[0]
+
+  @comments = @db.execute "SELECT * from Comments where post_id = ? order by id", [post_id]
+
+  erb :details
 end
 
-get '/' do
-  erb 'Can you handle a <a href="/secure/place">secret</a>?'
-end
+post '/details/:post_id' do
+  post_id = params[:post_id]
 
-get '/login/form' do
-  erb :login_form
-end
+  content = params[:content]
 
-post '/login/attempt' do
-  session[:identity] = params['username']
-  session[:identity] = params['password']
-  where_user_came_from = session[:previous_url] || '/'
-  redirect to where_user_came_from
-end
+  @db.execute "INSERT INTO Comments (content, created_date, post_id) values (?, datetime(), ?)", [content, post_id]
 
-get '/logout' do
-  session.delete(:identity)
-  erb "<div class='alert alert-message'>Logged out</div>"
-end
-
-get '/secure/place' do
-  erb 'This is a secret place that only <%=session[:identity]%> has access to!'
+  redirect to('/details/' + post_id)
 end
